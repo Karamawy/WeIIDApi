fe28eb24f01aa99da70ce5676d831ed911899f14
Abdelrahman Abdelwahab <abdelrahmankaram98@gmail.com>
2022-02-08 18:35:53 +0100

    Alogrithm and Model

---
+++ src/main/java/com/welld/patternrecognition/Api/PointController.java
@@ -0,0 +1,33 @@
+package com.welld.patternrecognition.Api;
+
+import com.welld.patternrecognition.Model.Point;
+import com.welld.patternrecognition.Service.PointsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PointController {
+    @Autowired
+    private PointsService pointsService;
+
+    @GetMapping("/space")
+    public List<Point> all_points(){
+        return pointsService.getAllPointsList();
+    }
+
+    @PostMapping("/point")
+    public void addPoint(@RequestBody Point point){
+        pointsService.addPoint(point);
+    }
+
+    @DeleteMapping("/space")
+    public void deleteSpace(){
+        pointsService.deleteSpace();
+    }
+    @GetMapping("/lines/{n}")
+    public List<List<Point>> getAllLines(@PathVariable("n") int n){
+        pointsService.getAllLines(n);
+    }
+}
---
+++ src/main/java/com/welld/patternrecognition/Service/PointsService.java
@@ -0,0 +1,55 @@
+package com.welld.patternrecognition.Service;
+
+import com.welld.patternrecognition.Model.Point;
+import org.springframework.stereotype.Service;
+
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class PointsService {
+    private List<Point> pointList = new ArrayList<>();
+
+
+    public List<Point> getAllPointsList(){
+        return pointList;
+    }
+
+    private Point2D modelToJavaPoint(Point p){
+        return new Point2D.Double(p.getX(),p.getY());
+    }
+    private HashMap<Line2D,List<Point>> pointsOnLine;
+
+    public List<List<Point>> getAllLines(int n){
+        List<Line2D> lines= new ArrayList<>();
+        for(int i=0;i<pointList.size();i++){
+            for(int j=i+1;j<pointList.size();j++){
+                Point2D point1= modelToJavaPoint(pointList.get(i));
+                Point2D point2= modelToJavaPoint(pointList.get(j));
+                Line2D line = new Line2D.Double(point1,point2);
+                lines.add(line);
+            }
+        }
+        for(Line2D line : lines){
+            List<Point> pointsInline = new ArrayList<>();
+            for(Point p : pointList){
+                Point2D temp=modelToJavaPoint(p);
+                if(line.contains(temp)==true){
+                    pointsInline.add(p);
+                }
+            }
+            pointsOnLine.put(line,pointsInline);
+        }
+        return pointsOnLine.values().stream().filter(l->l.size()>=n).collect(Collectors.toList());
+    }
+
+
+    public void addPoint(Point point) {
+        pointList.add(point);
+    }
+    public void deleteSpace() {
+        pointList.clear();
+    }
+}
