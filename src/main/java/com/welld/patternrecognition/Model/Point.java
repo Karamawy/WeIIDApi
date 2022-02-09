8938b3f5a4c4b5c62a6860894ccb3bdb941096e9
Abdelrahman Karam <helldash2@yahoo.com>
2022-02-09 16:14:26 +0100

    validation

--- src/main/java/com/welld/patternrecognition/Model/Point.java
+++ src/main/java/com/welld/patternrecognition/Model/Point.java
@@ -1,7 +1,15 @@
 package com.welld.patternrecognition.Model;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class Point {
+    @NotNull
+    @Pattern(regexp="/^[0-9]+(\\\\.[0-9]+)?$")
     private final double x;
+    
+    @NotNull
+    @Pattern(regexp="/^[0-9]+(\\\\.[0-9]+)?$")
     private final double y;
 
     public Point( double x, double y){
